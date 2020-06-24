99048ea03d722d744a9fc241e188a963ca6fe68e
Kris Bastiani <kris.bastiani@gmail.com>
2020-06-24 18:16:17 +1000

    Set up ESLint preferences

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+	env: {
+		browser: true,
+		jest: true,
+	},
+	extends: [
+		'airbnb-base',
+		'plugin:react/recommended',
+		'plugin:security/recommended',
+		'react-app',
+	],
+	globals: {
+		afterEach: true,
+		beforeEach: true,
+		browser: true,
+		by: true,
+		console: true,
+		describe: true,
+		document: true,
+		element: true,
+		ENV: true,
+		expect: true,
+		it: true,
+		jest: true,
+		window: true,
+	},
+	parserOptions: { ecmaVersion: 10 },
+	plugins: [
+		'import',
+		'jest',
+		'react',
+		'security',
+	],
+	rules: {
+		'arrow-parens': ['error', 'as-needed'],
+		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+		indent: ['error', 'tab'],
+		'jsx-quotes': ['error', 'prefer-single'],
+		'no-mixed-operators': [
+			'error',
+			{
+				groups: [
+					['+', '-', '*', '/', '%', '**'],
+					['&', '|', '^', '~', '<<', '>>', '>>>'],
+					['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+					['&&', '||'],
+					['in', 'instanceof'],
+				],
+			},
+		],
+		'no-multiple-empty-lines': [
+			'error',
+			{
+				max: 1,
+				maxEOF: 1,
+			},
+		],
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		'no-unused-expressions': ['error', {
+			allowShortCircuit: true,
+			allowTernary: true,
+		}],
+		'react/prop-types': 0,
+		'security/detect-object-injection': 0,
+	},
+	settings: {
+		react: { version: 'detect' },
+	},
+};
