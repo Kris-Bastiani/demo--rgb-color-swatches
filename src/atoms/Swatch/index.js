7d40c420e8fac15a126d509ac876ed7c2fa0cfcf
Kris Bastiani <kris.bastiani@gmail.com>
2020-06-24 20:53:00 +1000

    Select color from Swatch on click

--- src/atoms/Swatch/index.js
+++ src/atoms/Swatch/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import './Swatch.scss';
 
-const Swatch = ({ red, green, blue }) => {
+const Swatch = ({
+	red,
+	green,
+	blue,
+	setSelected,
+}) => {
 	const rgb = `rgb(${red}, ${green}, ${blue})`;
 
 	return (
-		<button className='swatch'>
+		<button className='swatch' onClick={() => setSelected(rgb)}>
 			<div className='swatch__color' style={{ backgroundColor: rgb }}></div>
 			<p className='swatch__label'>{rgb}</p>
 		</button>
--- src/organisms/App/index.js
+++ src/organisms/App/index.js
@@ -5,23 +5,25 @@ import getData from '../../helpers/getData';
 import Button from '../../atoms/Button';
 import Swatch from '../../atoms/Swatch';
 
-const updateColors = async setColors => {
+const updateColors = async (setColors, setSelected) => {
 	const response = await getData(`${API_BASE_URL}/list`);
-	return setColors(response);
+	setColors(response);
+	setSelected('none');
 };
 
 const App = () => {
 	const [colors, setColors] = useState([]);
+	const [selected, setSelected] = useState('none');
 
-	useEffect(() => { updateColors(setColors); }, []);
+	useEffect(() => { updateColors(setColors, setSelected); }, []);
 
 	return (
 		<div className='App'>
-			<Button label='Refresh' onClick={() => updateColors(setColors)} />
-			<br />
+			<Button label='Refresh' onClick={() => updateColors(setColors, setSelected)} />
+			<p>Selected: {selected}</p>
 			{colors.map((color, index) => {
 				const { red, green, blue } = color.components;
-				return <Swatch key={index} red={red} green={green} blue={blue} />;
+				return <Swatch key={index} red={red} green={green} blue={blue} setSelected={setSelected} />;
 			})}
 		</div>
 	);
