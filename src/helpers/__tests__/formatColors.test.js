fd70d70b822df391f9ecdf7678d64d7c91bf3883
Kris Bastiani <kris.bastiani@gmail.com>
2020-06-25 10:26:15 +1000

    Add BRGB conversion to formatColors() and add test

--- src/helpers/__tests__/formatColors.test.js
+++ src/helpers/__tests__/formatColors.test.js
@@ -1,9 +1,16 @@
 import RESPONSE from '../../__mocks__/list.json';
 import EXPECTED from '../../__mocks__/list__formatted.json';
+import RESPONSE_EXT from '../../__mocks__/list-extended.json';
+import EXPECTED_EXT from '../../__mocks__/list-extended__formatted.json';
+
 import formatColors from '../formatColors';
 
 describe('formatColors', () => {
 	it('converts all colors from the `list` endpoint to RGB objects', () => {
 		expect(formatColors(RESPONSE)).toEqual(EXPECTED);
+	});
+
+	it('converts all colors from the `list-extended` endpoint to RGB objects', () => {
+		expect(formatColors(RESPONSE_EXT)).toEqual(EXPECTED_EXT);
 	});
 });
--- src/helpers/formatColors.js
+++ src/helpers/formatColors.js
@@ -1,7 +1,10 @@
+import brgbToRgb from './brgbToRgb';
 import hslToRgb from './hslToRgb';
 
 export default colors => colors.map(({ kind, components }) => {
 	switch (kind) {
+	case 'brgb':
+		return brgbToRgb(components.bred, components.bgreen, components.bblue);
 	case 'hsl':
 		return hslToRgb(components.hue, components.saturation, components.lightness);
 	default:
