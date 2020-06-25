05c31723315122c6f54f98c883497e7358a0a3de
Kris Bastiani <kris.bastiani@gmail.com>
2020-06-25 10:18:21 +1000

    Minor: typo

--- src/helpers/__tests__/hslToRgb.test.js
+++ src/helpers/__tests__/hslToRgb.test.js
@@ -1,7 +1,7 @@
 import hslToRgb from '../hslToRgb';
 
 describe('HSL to RGB', () => {
-	it('takes HSl values as argumants and returns an object with correct RGB values', () => {
+	it('takes HSl values as arguments and returns an object with correct RGB values', () => {
 		expect(hslToRgb(147, 50, 47)).toEqual({
 			red: 60,
 			green: 180,
